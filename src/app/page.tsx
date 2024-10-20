aaedd18ecfa523a643133ace5bec6ae7ebdbcc9a
Ghulam Muneer uddin <159724044+00Muneer12@users.noreply.github.com>
2024-10-20 13:39:08 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,61 @@
-import Image from "next/image";
+"use client";
+import React from "react";
+import StudentCard from  "./components/StudentCard";
+
+const students = [
+  {
+    name: "Ghulam Muneer uddin",
+    age: 26,
+    rollNumber: "abc2121",
+    className: "GIAIC",
+    imageUrl: "/2.jpg",
+    gender: "Male",
+    city: "Karachi",
+    phoneNumber: "03332663014",
+    address: "123 Main St, Gulberg, Karachi",
+  },
+  
+  { 
+    name: "Muhammad Saif ur Rehman",
+    age: 16,
+    rollNumber: "xyz2324",
+    className: "BBA",
+    imageUrl: "/1.jpg",
+    gender: "Male",
+    city: "Hyderabad",
+    phoneNumber: "03318490932",
+    address: "H no 3, Latifabad 9,Hyderabad",
+    },
+
+  {
+    name: "Muhammad Hamood ur Rehman",
+    age: 12,
+    rollNumber: "klm4345",
+    className: "BSIT",
+    imageUrl: "/3.jpg",
+    gender: "Male",
+    city: "Karachi",
+    phoneNumber: "03142345187",
+    address: "Malir town, Khokhrapar 1, Karachi",
+  },
+];
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div className="home ">
+      
+      <h1 className="title text-center text-bold text-5xl text-transform: uppercase card-align: center">Student ID Cards</h1>
+      <div className="student-cards">
+        {students.map((student, index) => (
+          <StudentCard key={index} student={student} />
+        ))}
+      </div>
+ <div>
+        <h2 className="text-center text-3xl font-bold text-b text-underline-offset: 8px text-transform: uppercase text-black-800 underline hover:underline-offset-4">
+        Created by :- Ghulam Muneer Uddin
+        </h2>
+      </div>
     </div>
+
   );
-}
+} 
